466150b85764cfe088e2f570c7ba1320bfb29ef2
Aryan Sindhi <37263346+aryansindhi18@users.noreply.github.com>
2024-03-25 01:01:05 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -40,7 +40,7 @@ const options = {
           },
           security: [{ bearerAuth: [] }]
     },
-    apis: ["./index.js","./routes/index.js","./routes/account.js","./routes/user.js"]
+    apis: [path.join(process.cwd(),"./index.js","./routes/index.js","./routes/account.js","./routes/user.js"]
 }
 
 const swaggerSpec = swaggerJSDoc(options);
