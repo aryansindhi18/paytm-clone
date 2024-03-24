c499341f4dd18adac26f7e241f90dcd2a3a1392a
Aryan Sindhi <37263346+aryansindhi18@users.noreply.github.com>
2024-03-25 00:50:02 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,14 @@ app.use(express.json())
 
 
 const options = {
+    customCss:
+      '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+    customCssUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
+    ],
     definition: {
         openapi: "3.0.0",
         info: {
