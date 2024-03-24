0ec4fee60ad296a2d1a0f38ea5e02d8e35acf9a3
Aryan Sindhi <37263346+aryansindhi18@users.noreply.github.com>
2024-03-25 00:43:50 +0530

    CHanged base url in index.js for swaggerUI

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const options = {
         },
         servers: [
             {
-                url:"http://localhost:3000"
+                url:"https://paytm-clone-zeta.vercel.app/"
             }
         ],
         components: {
