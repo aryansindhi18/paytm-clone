90a23956cc2dc09e3da48f7f9d479ca45b445d8c
aryansindhi18 <aryansindhi18@gmail.com>
2024-03-30 09:47:19 +0530

    added forgot pwd ui on signin page

--- frontend/src/components/InputComponent.jsx
+++ frontend/src/components/InputComponent.jsx
@@ -1,7 +1,9 @@
-export function InputComponent({label,placeholder}){
+export function InputComponent({label,placeholder,label2}){
     return <div>
-        <div className=" text-sm  font-medium text-left py-2">
-            {label}
+        <div className=" text-sm  font-medium text-left py-2 flex justify-between">
+            <div className="flex flex-col justify-center">{label}</div>
+            {{label2} && <div className="pointer underline cursor-pointer
+             hover:text-slate-600 flex flex-col justify-center">{label2}</div>}
         </div>
         <input type="text" placeholder={placeholder} className=" w-full px-2 py-1 border rounded border-slate-200"/>
     </div>
